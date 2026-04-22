f20a99723b0bb4dd5cdd0801e7193b2e20e951d6
Yi Pan <ypan1988@users.noreply.github.com>
2026-04-22 12:28:27 +0100

    add comments

--- include/numcxx.h
+++ include/numcxx.h
@@ -738,6 +738,8 @@ public:
   auto extents() const { return span_.extents(); }
   size_t size()  const { return span_.size(); }
 
+  // Returns a const reference to the underlying data handle.
+  // This is an advanced interface for interoperability with std::mdspan.
   constexpr auto data_handle() const noexcept { return span_.data_handle(); }
   // clang-format on
 };
