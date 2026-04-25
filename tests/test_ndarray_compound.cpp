532d0177387f5d04d66536a3a3e7a6cb0a1f6430
Yi Pan <ypan1988@users.noreply.github.com>
2026-04-25 21:03:25 +0100

    add unittest for compound assign

---
+++ tests/test_ndarray_compound.cpp
@@ -0,0 +1,42 @@
+#include <gtest/gtest.h>
+#include "numcxx.h"
+
+TEST(NdarrayScalarCompoundAssign, AddAssign) {
+    numcxx::dmat A(2, 3);
+    std::iota(A.data(), A.data() + A.size(), 1);
+    A += 1.0;
+    double expected[] = {2,3,4,5,6,7};
+    for (size_t i = 0; i < A.size(); ++i) {
+        EXPECT_DOUBLE_EQ(A.data()[i], expected[i]);
+    }
+}
+
+TEST(NdarrayScalarCompoundAssign, SubAssign) {
+    numcxx::dmat A(2, 3);
+    std::iota(A.data(), A.data() + A.size(), 1);
+    A -= 1.0;
+    double expected[] = {0,1,2,3,4,5};
+    for (size_t i = 0; i < A.size(); ++i) {
+        EXPECT_DOUBLE_EQ(A.data()[i], expected[i]);
+    }
+}
+
+TEST(NdarrayScalarCompoundAssign, MulAssign) {
+    numcxx::dmat A(2, 3);
+    std::iota(A.data(), A.data() + A.size(), 1);
+    A *= 2.0;
+    double expected[] = {2,4,6,8,10,12};
+    for (size_t i = 0; i < A.size(); ++i) {
+        EXPECT_DOUBLE_EQ(A.data()[i], expected[i]);
+    }
+}
+
+TEST(NdarrayScalarCompoundAssign, DivAssign) {
+    numcxx::dmat A(2, 3);
+    std::iota(A.data(), A.data() + A.size(), 1);
+    A /= 2.0;
+    double expected[] = {0.5,1.0,1.5,2.0,2.5,3.0};
+    for (size_t i = 0; i < A.size(); ++i) {
+        EXPECT_DOUBLE_EQ(A.data()[i], expected[i]);
+    }
+}
